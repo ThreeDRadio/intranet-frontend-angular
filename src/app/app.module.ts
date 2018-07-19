e384e21cdcf51bf80ae1fecab56f8746f4f26c46
Michael Marner <michael@20papercups.net>
2018-07-19 17:09:05 +0930

    Fix loading in template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
@@ -9,16 +8,17 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { IntranetService, API_URL } from 'app/services/intranet.service';
+import { GUARDS } from 'app/guards';
+import { PIPES } from 'app/pipes';
+import { Id3Service } from 'app/services/id3.service';
+import { API_URL, IntranetService } from 'app/services/intranet.service';
 
 import { AppComponent } from './app.component';
 import { ROUTES } from './app.routes';
 import { COMPONENTS } from './components';
+import { MaterialModule } from './material.module';
 import { PAGES } from './pages';
 import * as Store from './store';
-import { GUARDS } from 'app/guards';
-import { Id3Service } from 'app/services/id3.service';
-import { PIPES } from 'app/pipes';
 
 @NgModule({
   declarations: [AppComponent, ...COMPONENTS, ...PAGES, ...PIPES],
