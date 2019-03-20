e7e1551bafc9f8905eeb661599a8b4d871e4849f
Michael Marner <michael@20papercups.net>
2019-03-20 22:00:33 +1030

    Only show upload link if user is in uploaders group

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { RequestAuthLogoutAction } from '../../store';
+import { Groups } from 'app/constants';
 
 @Component({
   selector: 'dashboard',
@@ -9,6 +10,8 @@ import { RequestAuthLogoutAction } from '../../store';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  public groups = Groups;
+
   constructor(private store: Store<any>) {}
 
   public logout() {
