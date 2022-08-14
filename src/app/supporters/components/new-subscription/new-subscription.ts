316b04cd077eb8f58183d6bc90bbfdfbf7c5056a
Michael Marner <michael@20papercups.net>
2022-08-15 08:55:06 +0930

    Send sub expiry as a yyyy-mm-dd string, not a date object

--- src/app/supporters/components/new-subscription/new-subscription.ts
+++ src/app/supporters/components/new-subscription/new-subscription.ts
@@ -1,27 +1,25 @@
-import { Component, Inject, Optional } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, Inject, Optional } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
-import * as moment from 'moment-timezone';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import * as moment from "moment-timezone";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-new-subscription',
-  templateUrl: './new-subscription.html',
-  styleUrls: ['./new-subscription.scss']
+  selector: "app-new-subscription",
+  templateUrl: "./new-subscription.html",
+  styleUrls: ["./new-subscription.scss"],
 })
 export class NewSubscriptionComponent {
   form = new FormGroup({
-    transaction_type: new FormControl('', Validators.required),
+    transaction_type: new FormControl("", Validators.required),
     expires_at: new FormControl(
-      moment()
-        .add(1, 'years')
-        .format('YYYY-MM-DD'),
+      moment().add(1, "years").format("YYYY-MM-DD"),
       Validators.required
     ),
     payment_processed: new FormControl(false),
     pack_sent: new FormControl(false),
-    shipping: new FormControl('', Validators.required),
-    note: new FormControl()
+    shipping: new FormControl("", Validators.required),
+    note: new FormControl(),
   });
 
   constructor(
@@ -34,6 +32,15 @@ export class NewSubscriptionComponent {
   }
 
   addSubscription() {
-    this.dialogRef.close(this.form.value);
+    const subDetails = {
+      ...this.form.value,
+      // the date picker returns a date object, that needs to be converted
+      // to YYYY-MM-DD and unfortunately nothing in the effect knows how
+      // to deal with the data at all, so we're doing it here
+      expires_at: moment(this.form.controls["expires_at"].value).format(
+        "YYYY-MM-DD"
+      ),
+    };
+    this.dialogRef.close(subDetails);
   }
 }
