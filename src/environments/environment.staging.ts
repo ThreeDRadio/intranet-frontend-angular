7a60ae9e153015381adce4813374a4f79f9a2c4b
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-06-12 18:15:50 +0930

    Apply a different style to the staging configuration.

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
   api: 'https://dockerhost01v.threedradio.com:8001/backend',
-  rollbarToken: 'fcd5a866ec5d4990a8ccb68076921044',
-  themeColor: '#ff9292' // Dark green
+  rollbarToken: 'fcd5a866ec5d4990a8ccb68076921044'
 };
