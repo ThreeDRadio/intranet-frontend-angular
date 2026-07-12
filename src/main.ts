ecbbae5e998e59c4b6c75ea53b7496f8fed1f7de
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-07-13 07:40:46 +0930

    Upgrade to sentry 8 (#75)
    * Upgrade to sentry 8
    
    * Remove unused import.

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,17 @@ import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import * as Sentry from "@sentry/angular";
-import { Integrations } from "@sentry/tracing";
 
 import { AppModule } from "app/app.module";
 import { environment } from "environments/environment";
 
 Sentry.init({
-  dsn:
-    "https://f972382f58e44de9b3468f7c896e10d5@o120815.ingest.sentry.io/5712655",
+  dsn: "https://f972382f58e44de9b3468f7c896e10d5@o120815.ingest.sentry.io/5712655",
   integrations: [
     // Registers and configures the Tracing integration,
     // which automatically instruments your application to monitor its
     // performance, including custom Angular routing instrumentation
-    new Integrations.BrowserTracing({
-      tracingOrigins: ["localhost", "https://intranet.threedradio.com/backend"],
-      routingInstrumentation: Sentry.routingInstrumentation,
-    }),
+    Sentry.browserTracingIntegration,
   ],
 
   // Set tracesSampleRate to 1.0 to capture 100%
