4088aa96d9e26138546b2a3a8959e01a43b3c3e4
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-07-13 06:39:19 +0930

    Upgrade to angular 16 (#69)
    * Upgrade to angular 16
    
    * Update sentry to version 8
    
    * Formatting based on reviewdog feedback.
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Formatting based on reviewdog feedback.
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Formatting
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/app/guards/authenticated.guard.ts
+++ src/app/guards/authenticated.guard.ts
@@ -1,23 +1,26 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
-import * as selectors from '../store/selectors/auth.selectors';
+import * as selectors from "../store/selectors/auth.selectors";
 
 @Injectable()
-export class AuthenticatedGuard implements CanActivate {
-  constructor(private store: Store<any>, private router: Router) {}
+export class AuthenticatedGuard {
+  constructor(
+    private store: Store<any>,
+    private router: Router,
+  ) {}
   canActivate(): Observable<boolean> {
     return this.store.select(selectors.getAuth).pipe(
-      map(token => {
+      map((token) => {
         if (!token) {
-          this.router.navigate(['login']);
+          this.router.navigate(["login"]);
           return false;
         }
         return true;
-      })
+      }),
     );
   }
 }
--- src/app/guards/ready.guard.ts
+++ src/app/guards/ready.guard.ts
@@ -1,19 +1,19 @@
-import '../rxjs';
+import "../rxjs";
 
-import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { Store } from '@ngrx/store';
-import * as selectors from '../store/selectors/auth.selectors';
-import { Observable } from 'rxjs';
-import { map, skipWhile } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+
+import { Store } from "@ngrx/store";
+import * as selectors from "../store/selectors/auth.selectors";
+import { Observable } from "rxjs";
+import { map, skipWhile } from "rxjs/operators";
 
 @Injectable()
-export class ReadyGuard implements CanActivate {
+export class ReadyGuard {
   constructor(private store: Store<any>) {}
   canActivate(): Observable<boolean> {
     return this.store.select(selectors.isReady).pipe(
-      skipWhile(val => !val),
-      map(() => true)
+      skipWhile((val) => !val),
+      map(() => true),
     );
   }
 }
