14fab405b4a1f23ae0c3eaa4412b27afe34ca961
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-07-13 06:22:41 +0930

    Upgrade to angular 16

--- src/app/guards/authenticated.guard.ts
+++ src/app/guards/authenticated.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
 import * as selectors from '../store/selectors/auth.selectors';
 
 @Injectable()
-export class AuthenticatedGuard implements CanActivate {
+export class AuthenticatedGuard  {
   constructor(private store: Store<any>, private router: Router) {}
   canActivate(): Observable<boolean> {
     return this.store.select(selectors.getAuth).pipe(
--- src/app/guards/ready.guard.ts
+++ src/app/guards/ready.guard.ts
@@ -1,14 +1,14 @@
 import '../rxjs';
 
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+
 import { Store } from '@ngrx/store';
 import * as selectors from '../store/selectors/auth.selectors';
 import { Observable } from 'rxjs';
 import { map, skipWhile } from 'rxjs/operators';
 
 @Injectable()
-export class ReadyGuard implements CanActivate {
+export class ReadyGuard  {
   constructor(private store: Store<any>) {}
   canActivate(): Observable<boolean> {
     return this.store.select(selectors.isReady).pipe(
