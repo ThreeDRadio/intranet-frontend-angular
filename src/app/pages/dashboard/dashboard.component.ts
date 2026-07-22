616ad3130ad16dbfada4c0945600ea16677cce54
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-07-22 18:51:56 +0930

    105 add current version display to the dashboard sidenav (#106)
    * Update the package's version number.
    
    * Increment the version number for the next update.
    
    * Add version number display to the dashboard component.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,27 +1,55 @@
-import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Component } from "@angular/core";
+import { Store } from "@ngrx/store";
 
-import { RequestAuthLogoutAction } from '../../store';
-import { Groups } from 'app/constants';
-import { TopNavComponent } from '../../components/topnav/topnav.component';
-import { MatSidenavContainer, MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
-import { MatNavList, MatListItem, MatListItemIcon, MatListItemTitle, MatListSubheaderCssMatStyler } from '@angular/material/list';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { MatIcon } from '@angular/material/icon';
-import { InGroupDirective } from '../../directives/in-group.directive';
-import { MatToolbar } from '@angular/material/toolbar';
-import { PlayControllerComponent } from '../../components/play-controller/play-controller.component';
+import { RequestAuthLogoutAction } from "../../store";
+import { Groups } from "app/constants";
+import { TopNavComponent } from "../../components/topnav/topnav.component";
+import {
+  MatSidenavContainer,
+  MatSidenav,
+  MatSidenavContent,
+} from "@angular/material/sidenav";
+import {
+  MatNavList,
+  MatListItem,
+  MatListItemIcon,
+  MatListItemTitle,
+  MatListSubheaderCssMatStyler,
+} from "@angular/material/list";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { MatIcon } from "@angular/material/icon";
+import { InGroupDirective } from "../../directives/in-group.directive";
+import { MatToolbar } from "@angular/material/toolbar";
+import { PlayControllerComponent } from "../../components/play-controller/play-controller.component";
+import packageJson from "../../../../package.json";
 
 @Component({
-    // dashboard is one word. No kebab case required. GO AWAY
-    // eslint-disable-next-line @angular-eslint/component-selector
-    selector: 'dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss'],
-    imports: [TopNavComponent, MatSidenavContainer, MatSidenav, MatNavList, MatListItem, RouterLink, MatIcon, MatListItemIcon, MatListItemTitle, MatListSubheaderCssMatStyler, InGroupDirective, MatSidenavContent, RouterOutlet, MatToolbar, PlayControllerComponent]
+  // dashboard is one word. No kebab case required. GO AWAY
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: "dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"],
+  imports: [
+    TopNavComponent,
+    MatSidenavContainer,
+    MatSidenav,
+    MatNavList,
+    MatListItem,
+    RouterLink,
+    MatIcon,
+    MatListItemIcon,
+    MatListItemTitle,
+    MatListSubheaderCssMatStyler,
+    InGroupDirective,
+    MatSidenavContent,
+    RouterOutlet,
+    MatToolbar,
+    PlayControllerComponent,
+  ],
 })
 export class DashboardComponent {
   public groups = Groups;
+  public appVersion: string = packageJson.version;
 
   constructor(private store: Store<any>) {}
 
