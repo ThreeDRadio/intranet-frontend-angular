b8301e37026b85d1e3a5706f021b2b7cbe5532f9
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-07-29 07:00:16 +0930

    Update selector names.

--- src/app/components/playlist-header/playlist-header.component.ts
+++ src/app/components/playlist-header/playlist-header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "app-playlist-header.component",
+  selector: "app-playlist-header",
   imports: [],
   templateUrl: "./playlist-header.component.html",
   styleUrl: "./playlist-header.component.scss",
--- src/app/pages/recent-playlists/recent-playlists.component.ts
+++ src/app/pages/recent-playlists/recent-playlists.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "app-recent-playlists.component",
+  selector: "app-recent-playlists",
   imports: [],
   templateUrl: "./recent-playlists.component.html",
   styleUrl: "./recent-playlists.component.scss",
