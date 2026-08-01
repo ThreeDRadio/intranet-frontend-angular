1e8f1562189ec73dc2aec590ed0e913f83cd2cf5
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-08-01 15:47:46 +0930

    Update backend url for production.

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://intranet.threedradio.com/backend',
-  rollbarToken: 'fcd5a866ec5d4990a8ccb68076921044'
+  api: "https://intranet2.threedradio.com:8001/backend",
+  rollbarToken: "fcd5a866ec5d4990a8ccb68076921044",
 };
