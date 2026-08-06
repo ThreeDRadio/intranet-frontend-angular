81cd810d5ac3cc4a221933bf57954bd0aa37d957
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-08-06 16:18:27 +0930

    Update colour of fake chips. Change based on quota met status.

--- src/app/components/quota-indicator/quota-indicator.component.ts
+++ src/app/components/quota-indicator/quota-indicator.component.ts
@@ -1,6 +1,7 @@
-import { Component, input } from "@angular/core";
+import { Component, computed, input } from "@angular/core";
 import { MatIcon, MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
+import { QuotaResult } from "../../services/quota.service";
 
 @Component({
   selector: "app-quota-indicator",
@@ -11,7 +12,9 @@ import { DomSanitizer } from "@angular/platform-browser";
 export class QuotaIndicatorComponent {
   icon = input.required<string>();
   svg = input<boolean>();
-  quota = input.required();
+  quota = input.required<QuotaResult>();
+
+  readonly met = computed(() => this.quota().meets);
 
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon(
