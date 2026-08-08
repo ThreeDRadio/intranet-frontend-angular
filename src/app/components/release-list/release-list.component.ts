6c8d1e40bf3b95270c0d8d94380d37b92beb5a79
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-08-08 12:30:41 +0930

    Remove built-in uncopyable chips and replace them with a copyable ripoff

--- src/app/components/release-list/release-list.component.ts
+++ src/app/components/release-list/release-list.component.ts
@@ -1,19 +1,50 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Release } from 'app/models/release';
-import { MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow } from '@angular/material/table';
-import { MatSort, MatSortHeader } from '@angular/material/sort';
-import { MatChipListbox, MatChipOption } from '@angular/material/chips';
-import { DatePipe } from '@angular/common';
-import { MatIcon } from '@angular/material/icon';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Release } from "app/models/release";
+import {
+  MatTable,
+  MatColumnDef,
+  MatHeaderCellDef,
+  MatHeaderCell,
+  MatCellDef,
+  MatCell,
+  MatHeaderRowDef,
+  MatHeaderRow,
+  MatRowDef,
+  MatRow,
+} from "@angular/material/table";
+import { MatSort, MatSortHeader } from "@angular/material/sort";
+import {
+  MatChipListbox,
+  MatChipOption,
+  MatChipsModule,
+} from "@angular/material/chips";
+import { DatePipe } from "@angular/common";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
-    selector: 'app-release-list',
-    templateUrl: './release-list.component.html',
-    styleUrls: ['./release-list.component.scss'],
-    imports: [MatTable, MatSort, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatSortHeader, MatCellDef, MatCell, MatChipListbox, MatChipOption, MatIcon, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow, DatePipe]
+  selector: "app-release-list",
+  templateUrl: "./release-list.component.html",
+  styleUrls: ["./release-list.component.scss"],
+  imports: [
+    MatTable,
+    MatSort,
+    MatColumnDef,
+    MatHeaderCellDef,
+    MatHeaderCell,
+    MatSortHeader,
+    MatCellDef,
+    MatCell,
+    MatChipsModule,
+    MatIcon,
+    MatHeaderRowDef,
+    MatHeaderRow,
+    MatRowDef,
+    MatRow,
+    DatePipe,
+  ],
 })
 export class ReleaseListComponent {
-  displayedColumns = ['artist', 'title', 'year', 'createwhen', 'tags'];
+  displayedColumns = ["artist", "title", "year", "createwhen", "tags"];
   pageSizes = [10, 20, 50, 100];
 
   @Input()
