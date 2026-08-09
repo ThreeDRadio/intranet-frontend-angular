8f81afe873268d4b307999ccfd216698846574a0
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-08-09 17:07:52 +0930

    Finalize editing tools.

--- src/app/components/playlist-entry/playlist-entry.component.ts
+++ src/app/components/playlist-entry/playlist-entry.component.ts
@@ -3,10 +3,18 @@ import { PlaylistEntry } from "../../models/playlist-entry";
 import { QuotaCheckComponent } from "../quota-check/quota-check.component";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
 @Component({
   selector: "app-playlist-entry",
-  imports: [MatCardModule, MatIconModule, QuotaCheckComponent],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatIconModule,
+    QuotaCheckComponent,
+  ],
   templateUrl: "./playlist-entry.component.html",
   styleUrl: "./playlist-entry.component.scss",
 })
