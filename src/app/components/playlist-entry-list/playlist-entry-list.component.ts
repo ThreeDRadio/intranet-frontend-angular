56ccde57e2180584ddc2d75c152cc3694b8d4fb4
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-08-09 16:38:30 +0930

    Move cards into playlist entry list.

--- src/app/components/playlist-entry-list/playlist-entry-list.component.ts
+++ src/app/components/playlist-entry-list/playlist-entry-list.component.ts
@@ -1,18 +1,16 @@
 import { Component, input } from "@angular/core";
 import { Playlist } from "../../models/playlist";
 import { PlaylistEntry } from "../../models/playlist-entry";
-import { MatCardModule } from "@angular/material/card";
-import { MatIconModule } from "@angular/material/icon";
-import { QuotaCheckComponent } from "../quota-check/quota-check.component";
+import { PlaylistEntryComponent } from "../playlist-entry/playlist-entry.component";
 
 @Component({
   selector: "app-playlist-entry-list",
-  imports: [MatCardModule, MatIconModule, QuotaCheckComponent],
+  imports: [PlaylistEntryComponent],
   templateUrl: "./playlist-entry-list.component.html",
   styleUrl: "./playlist-entry-list.component.scss",
 })
 export class PlaylistEntryListComponent {
   playlist = input.required<Playlist>();
   entries = input.required<PlaylistEntry[]>();
-  action = input<string>();
+  action = input<string>("view");
 }
--- src/app/components/playlist-entry/playlist-entry.component.ts
+++ src/app/components/playlist-entry/playlist-entry.component.ts
@@ -1,9 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, input } from "@angular/core";
+import { PlaylistEntry } from "../../models/playlist-entry";
+import { QuotaCheckComponent } from "../quota-check/quota-check.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: "app-playlist-entry.component",
-  imports: [],
+  selector: "app-playlist-entry",
+  imports: [MatCardModule, MatIconModule, QuotaCheckComponent],
   templateUrl: "./playlist-entry.component.html",
   styleUrl: "./playlist-entry.component.scss",
 })
-export class PlaylistEntryComponent {}
+export class PlaylistEntryComponent {
+  input = input.required<PlaylistEntry>();
+  action = input<string>("view");
+}
