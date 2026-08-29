554dbecfbbf71624f62bd930352a3a1ed1e44e13
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-08-29 13:30:36 +0930

    List compact releases with standard material component.

--- src/app/components/release-list-compact-tracks/release-list-compact-tracks.component.ts
+++ src/app/components/release-list-compact-tracks/release-list-compact-tracks.component.ts
@@ -1,11 +1,19 @@
-import { Component, input } from "@angular/core";
+import { Component, computed, inject, input, OnInit } from "@angular/core";
+import { ReleaseStore } from "../../store/release.store";
+import { MatListModule } from "@angular/material/list";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 
 @Component({
   selector: "app-release-list-compact-tracks",
-  imports: [],
+  imports: [MatListModule, MatProgressBarModule],
   templateUrl: "./release-list-compact-tracks.component.html",
   styleUrl: "./release-list-compact-tracks.component.scss",
 })
 export class ReleaseListCompactTracksComponent {
   release = input.required<number>();
+  releaseStore = inject(ReleaseStore);
+
+  tracklist = computed(() =>
+    this.releaseStore.tracklistForId()(this.release()),
+  );
 }
