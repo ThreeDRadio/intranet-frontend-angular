8eb802fde16c4f05262d539a2b8931ee1d7b7206
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-09-05 06:22:32 +0930

    Formatting.

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,28 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { ReleaseActions } from 'app/store';
-import { UntypedFormGroup, UntypedFormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { ReleaseSelectors } from 'app/store/selectors/release.selectors';
-import { PageEvent, MatPaginator } from '@angular/material/paginator';
-import { Sort } from '@angular/material/sort';
-import { Router } from '@angular/router';
-import { MatFormField } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
-import { MatButton } from '@angular/material/button';
-import { AsyncPipe } from '@angular/common';
-import { MatProgressBar } from '@angular/material/progress-bar';
-import { ReleaseListComponent } from '../../components/release-list/release-list.component';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { ReleaseActions } from "app/store";
+import {
+  UntypedFormGroup,
+  UntypedFormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from "@angular/forms";
+import { Observable } from "rxjs";
+import { ReleaseSelectors } from "app/store/selectors/release.selectors";
+import { PageEvent, MatPaginator } from "@angular/material/paginator";
+import { Sort } from "@angular/material/sort";
+import { Router } from "@angular/router";
+import { MatFormField } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { MatButton } from "@angular/material/button";
+import { AsyncPipe } from "@angular/common";
+import { MatProgressBar } from "@angular/material/progress-bar";
+import { ReleaseListComponent } from "../../components/release-list/release-list.component";
 
 @Component({
-    selector: 'app-catalogue-search',
-    templateUrl: './search.component.html',
-    styleUrls: ['./search.component.scss'],
-    imports: [FormsModule, ReactiveFormsModule, MatFormField, MatInput, MatButton, MatProgressBar, ReleaseListComponent, MatPaginator, AsyncPipe]
+  selector: "app-catalogue-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.scss"],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatProgressBar,
+    ReleaseListComponent,
+    MatPaginator,
+    AsyncPipe,
+  ],
 })
 export class SearchComponent implements OnInit {
   form = new UntypedFormGroup({
-    search: new UntypedFormControl('', Validators.required)
+    search: new UntypedFormControl("", Validators.required),
   });
 
   loading$: Observable<boolean>;
@@ -33,7 +49,7 @@ export class SearchComponent implements OnInit {
 
   offset = 0;
   pageSize = 10;
-  ordering = '-createwhen';
+  ordering = "-createwhen";
 
   search() {
     if (this.form.valid) {
@@ -42,14 +58,14 @@ export class SearchComponent implements OnInit {
           ...this.form.value,
           offset: this.offset,
           limit: this.pageSize,
-          ordering: this.ordering
-        })
+          ordering: this.ordering,
+        }),
       );
     }
   }
 
   open(release) {
-    this.router.navigate(['releases', release.id]);
+    this.router.navigate(["releases", release.id]);
   }
 
   paginationChange(event: PageEvent) {
@@ -59,11 +75,14 @@ export class SearchComponent implements OnInit {
   }
 
   sort(event: Sort) {
-    console.log(event);
-    this.ordering = (event.direction === 'asc' ? '' : '-') + event.active;
+    this.ordering = (event.direction === "asc" ? "" : "-") + event.active;
     this.search();
   }
-  constructor(private store: Store<any>, private router: Router) {
+
+  constructor(
+    private store: Store<any>,
+    private router: Router,
+  ) {
     this.loading$ = this.store.select(ReleaseSelectors.isLoading);
     this.results$ = this.store.select(ReleaseSelectors.getAll);
     this.count$ = this.store.select(ReleaseSelectors.count);
