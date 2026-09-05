7447dcbd6c1ac5f23031396a4e8247e477654d1e
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-09-05 11:51:20 +0930

    Fix editor div background. Add callback for drop result.

--- src/app/components/playlist-entry-list/playlist-entry-list.component.ts
+++ src/app/components/playlist-entry-list/playlist-entry-list.component.ts
@@ -19,4 +19,8 @@ export class PlaylistEntryListComponent {
   // Outputs
   entryDeleted = output<number>();
   entrySaved = output<PlaylistEntry>();
+
+  entryDropped(event) {
+    console.log(event);
+  }
 }
