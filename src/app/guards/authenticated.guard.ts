338fe687fe13ca046a9f454cf5f7b285f9322b9a
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-09-12 09:33:58 +0930

    Switch to standard inject mechanism in authenticated guard.

--- src/app/guards/authenticated.guard.ts
+++ src/app/guards/authenticated.guard.ts
@@ -1,17 +1,18 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
 import * as selectors from "../store/selectors/auth.selectors";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class AuthenticatedGuard {
-  constructor(
-    private store: Store<any>,
-    private router: Router,
-  ) {}
+  private store = inject(Store<any>);
+  private router = inject(Router);
+  private http = inject(HttpClient);
+
   canActivate(): Observable<boolean> {
     return this.store.select(selectors.getAuth).pipe(
       map((token) => {
